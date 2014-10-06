9220b672f056fbbd588cc3ae534eb58732c9c816
branflakes15 <branflakes@outlook.com>
2014-10-05 22:06:32 -0700

    southeast.c program

--- southeast.c
+++ southeast.c
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 
-void go_south_east(int lat, int lon) //The Function for Latitude and Longitude
+void go_south_east(int* lat, int* lon) //The Function for Latitude and Longitude
 {
-	lat = lat - 1;
-	lon = lon + 1;
+	*lat = *lat - 1;
+	*lon = *lon + 1;
 }
 
 int main(void)
@@ -15,7 +15,7 @@ int main(void)
 	int latitude = 32; //Variables
 	int longitude = -64;
 
-	go_south_east(latitude, longitude);
+	go_south_east(&latitude, &longitude);
 
 
 	printf("Avast! Now at: [%i %i]\n", latitude, longitude);
